0a77c6365c81c556c811a0f61ea88714c1042bf0
Mykhailo Doloh <dologmihajlo31@gmail.com>
2025-09-04 21:20:51 +0300

    add tasc

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,12 @@
-// export const Sum = () => ();
+import React from 'react';
+
+// Named export of the Sum component
+export function Sum({ a = 0, b = 0 }) {
+  const sum = a + b;
+
+  return (
+    <p>
+      Sum of {a} and {b} is {sum}
+    </p>
+  );
+}
